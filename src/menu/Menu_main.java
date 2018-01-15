d59e4b5d378892ce64375c98c39a57ff14a55401
50616030 <50616030@50616030-PC>
2018-01-15 17:47:28 +0900

    50616030 Menu

---
+++ src/menu/Menu_main.java
@@ -0,0 +1,15 @@
+package menu;
+
+import menu.Menu_view;
+
+public class Menu_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Menu_view frame = new Menu_view();
+        frame.setBounds(5,5,700,455);
+        frame.setVisible(true);
+
+	}
+
+}
